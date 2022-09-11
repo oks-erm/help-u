d42b752c962092b926b119ff633a7a25095d5fb7
oks-erm <oks.erm.012@gmail.com>
2022-09-11 18:02:37 +0000

    Add back to the top button

--- main/models.py
+++ main/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import formats
-from datetime import datetime
 
 
 class ContactFormMessage(models.Model):
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -95,5 +95,18 @@
       })
     });
 
+    // back to the top
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+      const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+          backtotop.classList.add('active')
+        } else {
+          backtotop.classList.remove('active')
+        }
+      }
+      window.addEventListener('load', toggleBacktotop)
+      onscroll(document, toggleBacktotop)
+    }
     
   })()
