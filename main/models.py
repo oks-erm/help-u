ad5021a4ebdaf7fefdb582cc5a945ffdc007b437
oks-erm <oks.erm.012@gmail.com>
2022-09-11 17:52:49 +0000

    Wire up contact form

--- main/models.py
+++ main/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.utils import formats
+from datetime import datetime
 
 
-class ContactFormInquiry(models.Model):
+class ContactFormMessage(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     subject = models.CharField(max_length=80)
     message = models.CharField(max_length=2000)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Inquiry from {self.name}, subject: {self.subject}"
+        short_date = 'SHORT_DATETIME_FORMAT'
+        return (f"from {self.name} | subject: {self.subject} | "
+                f"created: {formats.date_format(self.date, short_date)}")
--- main/views.py
+++ main/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.contrib import messages
+from datetime import datetime
+from .models import ContactFormMessage
 
 
 def home_page(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        msg = request.POST.get('message')
+        # date = datetime.now()
+        print(name)
+        new = ContactFormMessage(
+            name=name,
+            email=email,
+            subject=subject,
+            message=msg,
+            # date=date,
+            )
+        new.save()
+
     return render(request, 'index.html')
