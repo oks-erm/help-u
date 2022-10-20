0d4d9beca49b4fa142288862193bebd910829376
oks-erm <oks.erm.012@gmail.com>
2022-10-20 22:46:15 +0000

    Add sending message and handling events in the consumers.py

--- messenger/consumers.py
+++ messenger/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import JsonWebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(JsonWebsocketConsumer):
@@ -15,10 +16,16 @@ class ChatConsumer(JsonWebsocketConsumer):
         print("Connected!")
         self.room_name = "home"
         self.accept()
+        # because group_add is asynchronous, it would not work
+        # without being converted to a synchronous function
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name,
+        )
         self.send_json(
             {
                 "type": "welcome_message",
-                "message": "Hey there! You've successfully connected!",
+                "message": "Hey, silly sausage! You've successfully connected!",
             }
         )
 
@@ -27,5 +34,18 @@ class ChatConsumer(JsonWebsocketConsumer):
         return super().disconnect(code)
 
     def receive_json(self, content, **kwargs):
-        print(content)
+        message_type = content["type"]
+        if message_type == "chat_message":
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_name,
+                {
+                    "type": "chat_message_echo",
+                    "name": content["name"],
+                    "message": content["message"],
+                },
+            )
         return super().receive_json(content, **kwargs)
+    
+    def chat_message_echo(self, event):
+        print(event)
+        self.send_json(event)
