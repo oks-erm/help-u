3ae166ddbbac1213aa52b19989f03b6b31427ab6
oks-erm <oks.erm.012@gmail.com>
2022-11-14 10:06:05 +0000

    Fix websoket urls

--- frontend_chat/src/components/Chat.tsx
+++ frontend_chat/src/components/Chat.tsx
@@ -12,7 +12,6 @@ import Card from 'react-bootstrap/Card';
 
 
 export default function Chat() {
-    const [welcomeMessage, setWelcomeMessage] = useState("");
     const [messageHistory, setMessageHistory] = useState<any>([]);
     const [message, setMessage] = useState("");
     const { conversationName } = useParams();
