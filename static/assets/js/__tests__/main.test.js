0bdc958cf6b20f01a0586f90d8f690b8a3d970b6
oks-erm <oks.erm.012@gmail.com>
2023-01-08 19:25:20 +0000

    Add tests for JS

---
+++ static/assets/js/__tests__/main.test.js
@@ -0,0 +1,75 @@
+import fs from 'fs';
+import path from 'path';
+const { JSDOM } = require('jsdom');
+global.Event = require('events').Event;
+
+// set up a simulated browser environment
+const htmlDocPath = path.join(
+  process.cwd(),
+  'static/assets/js/__tests__/html.html'
+);
+const html = fs.readFileSync(htmlDocPath).toString();
+const dom = new JSDOM(html);
+global.window = dom.window;
+global.document = dom.window.document;
+global.$ = require('jquery');
+
+const main = require('../main.js').default;
+
+beforeEach(() => {
+  const dom = new JSDOM(html);
+  global.window = dom.window;
+  global.document = dom.window.document;
+});
+
+describe('select', () => {
+  test('should select an element', () => {
+    const expected = document.querySelector('#hero');
+    const el = main.select('#hero');
+
+    expect(el).toEqual(expected);
+  });
+
+  test('should select multiple elements', () => {
+    const expected = [...document.querySelectorAll('.section-title')];
+    const el = main.select('.section-title', true);
+
+    expect(el).toEqual(expected);
+  });
+});
+
+describe('on', () => {
+  test('on adds an event listener to a single element', () => {
+    const listener = jest.fn();
+    const el = document.querySelector('#hero');
+
+    main.on('click', '#hero', listener, false);
+    el.click();
+
+    expect(listener).toHaveBeenCalled();
+  });
+
+  test('on adds an event listener to multiple elements', () => {
+    const expected = [...document.querySelectorAll('.section-title')];
+    const listener = jest.fn();
+    main.on('click', '.section-title', listener, true);
+    expected[0].click();
+    expected[1].click();
+
+    expect(listener).toHaveBeenCalledTimes(2);
+  });
+});
+
+describe('onscroll', () => {
+  test('onscroll should call the listener function when the element is scrolled', () => {
+    const listener = jest.fn();
+    const el = document.querySelector('#hero');
+
+    main.onscroll(el, listener);
+    const scrollEvent = new window.CustomEvent('scroll');
+    el.dispatchEvent(scrollEvent);
+
+    expect(listener).toHaveBeenCalled();
+  });
+});
+
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,146 +1,155 @@
-(function () {
-    "use strict";
+const main = (() => {
+  'use strict';
 
-    // select helper
-    const select = (el, all = false) => {
-      el = el.trim()
+  // select helper
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  // event listener setter
+  const on = (type, el, listener, all = false) => {
+    let selectElement = select(el, all);
+    if (selectElement) {
       if (all) {
-        return [...document.querySelectorAll(el)]
+        selectElement.forEach((e) => e.addEventListener(type, listener));
       } else {
-        return document.querySelector(el)
+        selectElement.addEventListener(type, listener);
       }
     }
+  };
 
-    // event listener setter
-    const on = (type, el, listener, all = false) => {
-      let selectElement = select(el, all)
-      if (selectElement) {
-        if (all) {
-          selectElement.forEach(e => e.addEventListener(type, listener))
-        } else {
-          selectElement.addEventListener(type, listener)
-        }
+  // listener for scroll
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener);
+  };
+
+  // header onscroll animation
+  let selectHeader = select('#header');
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled');
+      } else {
+        selectHeader.classList.remove('header-scrolled');
       }
-    }
+    };
+    window.addEventListener('load', headerScrolled);
+    onscroll(document, headerScrolled);
+  }
 
-    // listener for scroll
-    const onscroll = (el, listener) => {
-      el.addEventListener('scroll', listener)
-    }
-
-    // header onscroll animation
-    let selectHeader = select('#header')
-    if (selectHeader) {
-      const headerScrolled = () => {
-        if (window.scrollY > 100) {
-          selectHeader.classList.add('header-scrolled')
-        } else {
-          selectHeader.classList.remove('header-scrolled')
-        }
+  // logo onscroll animation
+  let selectLogo = select('#small-img-logo');
+  if (selectLogo) {
+    const logoScrolled = () => {
+      if (window.scrollY > 400) {
+        selectLogo.classList.add('small-logo-scrolled');
+      } else {
+        selectLogo.classList.remove('small-logo-scrolled');
       }
-      window.addEventListener('load', headerScrolled)
-      onscroll(document, headerScrolled)
-    }
+    };
+    window.addEventListener('load', logoScrolled);
+    onscroll(document, logoScrolled);
+  }
 
-    // logo onscroll animation
-    let selectLogo = select('#small-img-logo')
-    if (selectLogo) {
-      const logoScrolled = () => {
-        if (window.scrollY > 400) {
-          selectLogo.classList.add('small-logo-scrolled')
-        } else {
-          selectLogo.classList.remove('small-logo-scrolled')
-        }
+  // shevron arrow onscroll animation
+  let selectArrow = select('.arrow');
+  if (selectArrow) {
+    const arrowScrolled = () => {
+      if (window.scrollY > 80) {
+        selectArrow.classList.add('arrow-scrolled');
+      } else {
+        selectArrow.classList.remove('arrow-scrolled');
       }
-      window.addEventListener('load', logoScrolled)
-      onscroll(document, logoScrolled)
-    }
+    };
+    window.addEventListener('load', arrowScrolled);
+    onscroll(document, arrowScrolled);
+  }
 
-    // shevron arrow onscroll animation
-    let selectArrow = select('.arrow')
-    if (selectArrow) {
-      const arrowScrolled = () => {
-        if (window.scrollY > 80) {
-          selectArrow.classList.add('arrow-scrolled')
-        } else {
-          selectArrow.classList.remove('arrow-scrolled')
-        }
-      }
-      window.addEventListener('load', arrowScrolled)
-      onscroll(document, arrowScrolled)
-    }
+  // mobile navbar
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile');
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
 
-    // mobile navbar
-    on('click', '.mobile-nav-toggle', function (e) {
-      select('#navbar').classList.toggle('navbar-mobile')
-      this.classList.toggle('bi-list')
-      this.classList.toggle('bi-x')
-    })
-
-    // Preloader
-    let loader = select('#loader');
-    if (loader) {
-      window.addEventListener('load', () => {
-        loader.remove()
-      });
-    }
-
-    // Animation on scroll   
+  // Preloader
+  let loader = select('#loader');
+  if (loader) {
     window.addEventListener('load', () => {
-      AOS.init({
-        duration: 1000,
-        easing: 'ease-in-out',
-        once: true,
-        mirror: false
-      })
+      loader.remove();
     });
+  }
 
-    // back to the top
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-      const toggleBacktotop = () => {
-        if (window.scrollY > 100) {
-          backtotop.classList.add('active')
-        } else {
-          backtotop.classList.remove('active')
-        }
+  // Animation on scroll
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false,
+    });
+  });
+
+  // back to the top
+  let backtotop = select('.back-to-top');
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active');
+      } else {
+        backtotop.classList.remove('active');
       }
-      window.addEventListener('load', toggleBacktotop)
-      onscroll(document, toggleBacktotop)
-    }
-    
-    // navbar links active state on scroll
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-      let position = window.scrollY + 200
-      navbarlinks.forEach(navbarlink => {
-        if (!navbarlink.hash) return
-        let section = select(navbarlink.hash)
-        if (!section) return
-        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-          navbarlink.classList.add('active')
-        } else {
-          navbarlink.classList.remove('active')
-        }
-      })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
+    };
+    window.addEventListener('load', toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
 
-    $(".geo").mouseover(function () {
-      $(this).children(".preview").show();
-    }).mouseout(function () {
-      $(this).children(".preview").hide();
+  // navbar links active state on scroll
+  let navbarlinks = select('#navbar .scrollto', true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add('active');
+      } else {
+        navbarlink.classList.remove('active');
+      }
+    });
+  };
+  window.addEventListener('load', navbarlinksActive);
+  onscroll(document, navbarlinksActive);
+
+  $('.geo')
+    .mouseover(function () {
+      $(this).children('.preview').show();
+    })
+    .mouseout(function () {
+      $(this).children('.preview').hide();
     });
 
-    $(".alert").delay(3000).fadeOut(1000);
+  $('.alert').delay(3000).fadeOut(1000);
 
+  $('#buttonClose').on('click', function () {
+    $('.modal').hide();
+    $('.modal-backdrop').hide();
+  });
 
-    $("#buttonClose").on("click", function () {
-      $(".modal").hide();
-      $(".modal-backdrop").hide();
-  }); 
+  return {
+    select,
+    on,
+    onscroll,
+  };
+})();
 
-    })();
-
-  
+export default main;
