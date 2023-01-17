17e08f472444cea1db5d4637594cd77c6bd575a2
oks-erm <oks.erm.012@gmail.com>
2023-01-17 13:22:07 +0000

    Fix minor formatting issues in settings

--- help_u/settings.py
+++ help_u/settings.py
@@ -185,7 +185,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'  # noqa
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
