d7fc9f0237121d7e40827c7e178915a4b452826a
oks-erm <oks.erm.012@gmail.com>
2023-01-18 01:13:24 +0000

    Add error handling to messenger/api views

--- messenger/api/views.py
+++ messenger/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.response import Response
 from django.db.models import Q
+from django.db.utils import DatabaseError
+from rest_framework import status
 from messenger.api.pagynaters import MessagePagination
 from messenger.api.serialisers import ConversationSerializer, MessageSerializer
 from messenger.models import Conversation, Message
@@ -20,11 +23,23 @@ class ConversationViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
         Return a queryset of conversations that contain
         the current user's ID.
         """
-        # pylint: disable=no-member
-        queryset = Conversation.objects.filter(
-            name__contains=self.request.user.id
-        )
-        return queryset
+        try:
+            # pylint: disable=no-member
+            queryset = Conversation.objects.filter(
+                name__contains=self.request.user.id
+            )
+            return queryset
+        except DatabaseError as error:
+            print(f'Error connecting to the database: {error}')
+            return None
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if queryset is None:
+            return Response({"error": "Error connecting to the database"},
+                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def get_serializer_context(self):
         """
@@ -47,14 +62,26 @@ class MessageViewSet(ListModelMixin, GenericViewSet):
         Return a queryset of conversations that contain the current
         user's ID in their name.
         """
-        conversation_name = self.request.GET.get("conversation")
-        queryset = (
-            # pylint: disable=no-member
-            Message.objects.filter(
-                Q(to_user=self.request.user.id) |
-                Q(from_user=self.request.user.id)
+        try:
+            conversation_name = self.request.GET.get("conversation")
+            queryset = (
+                # pylint: disable=no-member
+                Message.objects.filter(
+                    Q(to_user=self.request.user.id) |
+                    Q(from_user=self.request.user.id)
+                )
+                .filter(conversation__name=conversation_name)
+                .order_by("-timestamp")
             )
-            .filter(conversation__name=conversation_name)
-            .order_by("-timestamp")
-        )
-        return queryset
+            return queryset
+        except DatabaseError as error:
+            print(f'Error connecting to the database: {error}')
+            return None
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if queryset is None:
+            return Response({"error": "Error connecting to the database"},
+                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
